97b379d14cd00232537302adaded899cab141f50
shivalakshmi <shivalakshmi.yella@gmail.com>
2015-07-21 14:52:19 +0530

    Minor changes

--- TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/model/Truck.java
+++ TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/model/Truck.java
@@ -1,6 +1,5 @@
 package com.fissionlabs.trucksfirst.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,62 +7,6 @@ import java.util.List;
  */
 public class Truck {
 
-    private ArrayList<TruckDetails> mTruckDetailsList = new ArrayList<>();
+    private List<TruckDetails> mTruckDetailsList;
 
-    class TruckDetails{
-        String vehicleNumber;
-        String vehicleRoute;
-        String eta;
-        String assignedPilot;
-        String pilotInHub;
-        String vehileInHub;
-
-        public String getAssignedPilot() {
-            return assignedPilot;
-        }
-
-        public void setAssignedPilot(String assignedPilot) {
-            this.assignedPilot = assignedPilot;
-        }
-
-        public String getEta() {
-            return eta;
-        }
-
-        public void setEta(String eta) {
-            this.eta = eta;
-        }
-
-        public String getPilotInHub() {
-            return pilotInHub;
-        }
-
-        public void setPilotInHub(String pilotInHub) {
-            this.pilotInHub = pilotInHub;
-        }
-
-        public String getVehicleNumber() {
-            return vehicleNumber;
-        }
-
-        public void setVehicleNumber(String vehicleNumber) {
-            this.vehicleNumber = vehicleNumber;
-        }
-
-        public String getVehicleRoute() {
-            return vehicleRoute;
-        }
-
-        public void setVehicleRoute(String vehicleRoute) {
-            this.vehicleRoute = vehicleRoute;
-        }
-
-        public String getVehileInHub() {
-            return vehileInHub;
-        }
-
-        public void setVehileInHub(String vehileInHub) {
-            this.vehileInHub = vehileInHub;
-        }
-    }
 }
---
+++ TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/model/TruckDetails.java
@@ -0,0 +1,61 @@
+package com.fissionlabs.trucksfirst.model;
+
+/**
+ * Created by Lakshmi on 21-07-2015.
+ */
+class TruckDetails{
+    String vehicleNumber;
+    String vehicleRoute;
+    String eta;
+    String assignedPilot;
+    String pilotInHub;
+    String vehicleInHub;
+
+    public String getAssignedPilot() {
+        return assignedPilot;
+    }
+
+    public void setAssignedPilot(String assignedPilot) {
+        this.assignedPilot = assignedPilot;
+    }
+
+    public String getEta() {
+        return eta;
+    }
+
+    public void setEta(String eta) {
+        this.eta = eta;
+    }
+
+    public String getPilotInHub() {
+        return pilotInHub;
+    }
+
+    public void setPilotInHub(String pilotInHub) {
+        this.pilotInHub = pilotInHub;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getVehicleRoute() {
+        return vehicleRoute;
+    }
+
+    public void setVehicleRoute(String vehicleRoute) {
+        this.vehicleRoute = vehicleRoute;
+    }
+
+    public String getVehicleInHub() {
+        return vehicleInHub;
+    }
+
+    public void setVehicleInHub(String vehicleInHub) {
+        this.vehicleInHub = vehicleInHub;
+    }
+}
