c63a30447269f7fefa523564bc92c5363424960d
Ashok <ashok.tallapelli@fissionlabs.com>
2015-07-24 14:26:13 +0530

    Added save menu item in checklist screen

--- TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/fragments/TFCheckListFragment.java
+++ TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/fragments/TFCheckListFragment.java
@@ -4,6 +4,9 @@ package com.fissionlabs.trucksfirst.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,13 +18,39 @@ import com.fissionlabs.trucksfirst.common.TFCommonFragment;
  */
 public class TFCheckListFragment extends TFCommonFragment {
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_check_list,container,false);
+        View view = inflater.inflate(R.layout.fragment_check_list, container, false);
 
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_check_list, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                //To do send checklist data to server.
+                getActivity().onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
 }
