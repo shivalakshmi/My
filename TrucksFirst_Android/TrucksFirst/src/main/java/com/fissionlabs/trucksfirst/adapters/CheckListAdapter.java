d587505ae83137bc93156afaabcaa0bfd61a429e
shivalakshmi <shivalakshmi.yella@gmail.com>
2015-07-31 16:27:48 +0530

    Email popup modifications.

--- TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/adapters/CheckListAdapter.java
+++ TrucksFirst_Android/TrucksFirst/src/main/java/com/fissionlabs/trucksfirst/adapters/CheckListAdapter.java
@@ -1,9 +1,12 @@
 package com.fissionlabs.trucksfirst.adapters;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +16,12 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.fissionlabs.trucksfirst.R;
 import com.fissionlabs.trucksfirst.pojo.Checklist;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CheckListAdapter extends BaseAdapter {
 
@@ -31,22 +34,19 @@ public class CheckListAdapter extends BaseAdapter {
 
     private LayoutInflater mInflater;
 
-    public CheckListAdapter(Context context,ArrayList<Checklist> ChecklistArrayList) {
-        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public CheckListAdapter(Context context, ArrayList<Checklist> ChecklistArrayList) {
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.mChecklistArrayList = ChecklistArrayList;
         this.context = context;
     }
 
     @Override
     public int getItemViewType(int position) {
-        if(position==12)
-        {
+        if (position == 12) {
             return TECHNICAL_HEADER;
-        }
-        else if(position<=11)
-        {
+        } else if (position <= 11) {
             return OPERATIONAL_ITEM;
-        }else {
+        } else {
             return TECHNICAL_ITEM;
         }
     }
@@ -77,7 +77,7 @@ public class CheckListAdapter extends BaseAdapter {
             holder = new ViewHolder();
             switch (type) {
                 case TECHNICAL_HEADER:
-                    convertView = mInflater.inflate(R.layout.technical_header,parent, false);
+                    convertView = mInflater.inflate(R.layout.technical_header, parent, false);
                     break;
                 case OPERATIONAL_ITEM:
                     convertView = mInflater.inflate(R.layout.operational_item, parent, false);
@@ -87,7 +87,7 @@ public class CheckListAdapter extends BaseAdapter {
                     holder.mRadioBtnNo = (RadioButton) convertView.findViewById(R.id.radio_btn_no);
                     holder.mImgPrint = (ImageView) convertView.findViewById(R.id.print_img);
                     holder.mImgEmail = (ImageView) convertView.findViewById(R.id.email_img);
-                    holder.mRadioGroup = (RadioGroup)convertView.findViewById(R.id.radio_group);
+                    holder.mRadioGroup = (RadioGroup) convertView.findViewById(R.id.radio_group);
 
                     break;
                 case TECHNICAL_ITEM:
@@ -99,7 +99,7 @@ public class CheckListAdapter extends BaseAdapter {
             assert convertView != null;
             convertView.setTag(holder);
         } else {
-            holder = (ViewHolder)convertView.getTag();
+            holder = (ViewHolder) convertView.getTag();
         }
 
         switch (type) {
@@ -115,12 +115,10 @@ public class CheckListAdapter extends BaseAdapter {
                     holder.mRadioBtnNo.setChecked(true);
                 }
                 holder.mImgEmail.setVisibility(View.INVISIBLE);
-                if(mChecklistArrayList.get(position).getChecklistItem().equalsIgnoreCase(context.getResources().getString(R.string.grn_bilti)))
-                {
+                if (mChecklistArrayList.get(position).getChecklistItem().equalsIgnoreCase(context.getResources().getString(R.string.grn_bilti))) {
                     holder.mImgEmail.setVisibility(View.VISIBLE);
                 }
-                if(position<6 || position==7)
-                {
+                if (position < 6 || position == 7) {
                     holder.mImgPrint.setVisibility(View.VISIBLE);
                 }
 
@@ -133,16 +131,12 @@ public class CheckListAdapter extends BaseAdapter {
                 });
 
 
-                holder.mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-                {
+                holder.mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        if(R.id.radio_btn_yes == checkedId)
-                        {
+                        if (R.id.radio_btn_yes == checkedId) {
                             mChecklistArrayList.get(position).setStatus(true);
-                        }
-                        else
-                        {
+                        } else {
                             mChecklistArrayList.get(position).setStatus(false);
                         }
                         notifyDataSetChanged();
@@ -174,27 +168,43 @@ public class CheckListAdapter extends BaseAdapter {
         ImageView mImgEmail;
         public RadioGroup mRadioGroup;
     }
+
     void sendEmail() {
-        String[] TO = {""};
-        String[] CC = {""};
-
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        Intent emailIntent = new Intent();
+        emailIntent.setAction(Intent.ACTION_SEND);
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Documents- GRN/Bilti");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
         emailIntent.setType("message/rfc822");
 
-        try {
+        PackageManager pm = context.getPackageManager();
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
 
-            context.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+        Intent openInChooser = Intent.createChooser(emailIntent, "Share Via");
+
+        List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+        List<LabeledIntent> intentList = new ArrayList<>();
+        for (int i = 0; i < resInfo.size(); i++) {
+            ResolveInfo ri = resInfo.get(i);
+            String packageName = ri.activityInfo.packageName;
+            if (packageName.contains("android.email")) {
+                emailIntent.setPackage(packageName);
+            } else if (packageName.contains("android.gm")) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Documents- GRN/Bilti");
+                intent.setType("message/rfc822");
+                intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+            }
         }
-        catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(context, "There is no email client installed.", Toast.LENGTH_SHORT).show();
-        }
+
+        LabeledIntent[] extraIntents = intentList.toArray(new LabeledIntent[intentList.size()]);
+
+        openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+        context.startActivity(openInChooser);
     }
 
 }
